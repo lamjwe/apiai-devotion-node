c758729f827a1acd48c01d1301d0412d9696139f
jonathanlam <lamjwe91@gmail.com>
2017-06-01 12:56:33 -0700

    Testing out actions-on-google

--- index.js
+++ index.js
@@ -14,9 +14,15 @@ const CHAPTER_ARGUMENT = 'Chapter';
 const START_VERSE_ARGUMENT = 'StartVerse';
 const END_VERSE_ARGUMENT = 'EndVerse';
 
+process.env.DEBUG = 'actions-on-google:*';
+const App = require('actions-on-google').ApiAiApp;
+
 restService.post('/hook', function (req, res) {
 
     console.log('hook request');
+    const app = new App({req, res});
+    console.log('Request headers: ' + JSON.stringify(request.headers));
+    console.log('Request body: ' + JSON.stringify(request.body));
 
     try {
         var speech = 'empty speech';
@@ -37,48 +43,55 @@ restService.post('/hook', function (req, res) {
                 //     speech += 'action: ' + requestBody.result.action;
                 // }
                 if (requestBody.result.action == GET_PASSAGE) {
-                    var baseurl = "https://bibles.org/v2/passages.js?q[]=";
-                    var query = makeQuery(requestBody.result);
-
-                    var url = baseurl + query;
-                    var auth = new Buffer('c1QoJ6WPjJGycevbco8vJcWrnQdAxO5n3bUN04jN' + ':' + 'X').toString('base64');
-                    request({
-                        url: url,
-                        headers: {
-                            'Authorization': 'Basic ' + auth
-                        },
-                        method: 'GET'
-                    }, function (error, response, body) {
-                        if (error) {
-                            console.log('Error sending message: ', error);
-                        } else if (response.body.error) {
-                            console.log('Error: ', response.body.error);
-                        }
-
-                        console.log("SUCCESS: ");
-                        console.log("+++++++++++++++++++++++++++");
-                        var obj = JSON.parse(body);
-                        var text = obj.response["search"].result.passages[0]["text"];
-
-                        console.log(text);
-                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-
-                        var strippedText = striptags(text);
-                        console.log(strippedText);
-
-                        return res.json({
-                            speech: speech,
-                            displayText: strippedText,
-                            source: 'apiai-devotion'
-                        });
-                    });
+                    
                 }
             }
         }
 
         console.log('result: ', speech);
 
-        return null;
+        //return null;
+        // Make a silly name
+        function getPassage (app) {
+            var baseurl = "https://bibles.org/v2/passages.js?q[]=";
+            var query = makeQuery(app);
+
+            var url = baseurl + query;
+            var auth = new Buffer('c1QoJ6WPjJGycevbco8vJcWrnQdAxO5n3bUN04jN' + ':' + 'X').toString('base64');
+            request({
+                url: url,
+                headers: {
+                    'Authorization': 'Basic ' + auth
+                },
+                method: 'GET'
+            }, function (error, response, body) {
+                if (error) {
+                    console.log('Error sending message: ', error);
+                } else if (response.body.error) {
+                    console.log('Error: ', response.body.error);
+                }
+
+                console.log("SUCCESS: ");
+                console.log("+++++++++++++++++++++++++++");
+                var obj = JSON.parse(body);
+                var text = obj.response["search"].result.passages[0]["text"];
+
+                console.log(text);
+                console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+                var strippedText = striptags(text);
+                console.log(strippedText);
+                app.tell('Here is the passage: ' + strippedText);
+            });
+            
+        }
+
+        let actionMap = new Map();
+        actionMap.set(GET_PASSAGE, getPassage);
+
+        app.handleRequest(actionMap);
+
+
     } catch (err) {
         console.error("Can't process request", err);
 
@@ -91,24 +104,23 @@ restService.post('/hook', function (req, res) {
     }
 });
 
-function makeQuery(result) {
-    var parameters = result.parameters;
-    var book = parameters[BOOK_ARGUMENT];
+function makeQuery(app) {
+    var book = app.getArgument(BOOK_ARGUMENT);
     if (!book) {
         return None;
     }
 
-    var chapter = parameters[CHAPTER_ARGUMENT];
+    var chapter = app.getArgument(CHAPTER_ARGUMENT);
     if (!chapter) {
         return None;
     }
 
-    var start_verse = parameters[START_VERSE_ARGUMENT];
+    var start_verse = app.getArgument(START_VERSE_ARGUMENT);
     if (!start_verse) {
         start_verse = "1"
     } 
     
-    var end_verse = parameters[END_VERSE_ARGUMENT];
+    var end_verse = app.getArgument(END_VERSE_ARGUMENT);
     if (!end_verse){
         end_verse = "-ff"
     }
