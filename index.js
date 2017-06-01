e8cebc88c4bbb1dd4413416b8b7cc39d81853cc4
jonathanlam <lamjwe91@gmail.com>
2017-06-01 00:36:21 -0700

    Testing response again

--- index.js
+++ index.js
@@ -58,7 +58,6 @@ restService.post('/hook', function (req, res) {
                         console.log("response.body ====> " + response.body);
                         console.log("==========================");
                         console.log("response.body[\"response\"] ====>" + response.body["response"]);
-                        console.log("response.body[\"response\"].search ====>" + response.body["response"].search);
                         console.log("response.body[\"response\"][\"search\"] ====>" + response.body["response"]["search"]);
                     });
                 }
