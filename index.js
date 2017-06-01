83c5eabffaf727c41422086790bca60a0aa68d52
jonathanlam <lamjwe91@gmail.com>
2017-06-01 14:14:15 -0700

    undo all changes up to before setting up action google

--- index.js
+++ index.js
@@ -14,76 +14,71 @@ const CHAPTER_ARGUMENT = 'Chapter';
 const START_VERSE_ARGUMENT = 'StartVerse';
 const END_VERSE_ARGUMENT = 'EndVerse';
 
-process.env.DEBUG = 'actions-on-google:*';
-const App = require('actions-on-google').ApiAiApp;
-
 restService.post('/hook', function (req, res) {
 
     console.log('hook request');
-    const app = new App({req, res});
-    console.log('Request headers: ' + JSON.stringify(req.headers));
-    console.log('Request body: ' + JSON.stringify(req.body));
 
     try {
-        // var speech = 'empty speech';
+        var speech = 'empty speech';
 
-        // if (req.body) {
-        //     var requestBody = req.body;
+        if (req.body) {
+            var requestBody = req.body;
 
-        //     console.log("RequestBody: " + JSON.stringify(requestBody));
-        //     if (requestBody.result) {
-        //         speech = '';
+            console.log("RequestBody: " + JSON.stringify(requestBody));
+            if (requestBody.result) {
+                speech = '';
 
-        //         if (requestBody.result.fulfillment) {
-        //             speech += requestBody.result.fulfillment.speech;
-        //             speech += ' ';
-        //         }
-        //     }
-        // }
+                if (requestBody.result.fulfillment) {
+                    speech += requestBody.result.fulfillment.speech;
+                    speech += ' ';
+                }
 
-        // console.log('result: ', speech);
+                // if (requestBody.result.action) {
+                //     speech += 'action: ' + requestBody.result.action;
+                // }
+                if (requestBody.result.action == GET_PASSAGE) {
+                    var baseurl = "https://bibles.org/v2/passages.js?q[]=";
+                    var query = makeQuery(requestBody.result);
 
-        //return null;
-        // Make a silly name
-        function getPassage(app) {
-            console.log("IN GETTING PASSAGE");
-            var baseurl = "https://bibles.org/v2/passages.js?q[]=";
-            app.tell('Here is the passage you are looking for : ');
-            var query = makeQuery(app);
+                    var url = baseurl + query;
+                    var auth = new Buffer('c1QoJ6WPjJGycevbco8vJcWrnQdAxO5n3bUN04jN' + ':' + 'X').toString('base64');
+                    request({
+                        url: url,
+                        headers: {
+                            'Authorization': 'Basic ' + auth
+                        },
+                        method: 'GET'
+                    }, function (error, response, body) {
+                        if (error) {
+                            console.log('Error sending message: ', error);
+                        } else if (response.body.error) {
+                            console.log('Error: ', response.body.error);
+                        }
 
-            var url = baseurl + query;
-            console.log("URL : " + url);
-            var auth = new Buffer('c1QoJ6WPjJGycevbco8vJcWrnQdAxO5n3bUN04jN' + ':' + 'X').toString('base64');
-            // request({
-            //     url: url,
-            //     headers: {
-            //         'Authorization': 'Basic ' + auth
-            //     },
-            //     method: 'GET'
-            // }, function (error, response, body) {
-            //     if (error) {
-            //         console.log('Error sending message: ', error);
-            //     } else if (response.body.error) {
-            //         console.log('Error: ', response.body.error);
-            //     }
+                        console.log("SUCCESS: ");
+                        console.log("+++++++++++++++++++++++++++");
+                        var obj = JSON.parse(body);
+                        var text = obj.response["search"].result.passages[0]["text"];
 
-            //     console.log("SUCCESS: ");
-            //     console.log("+++++++++++++++++++++++++++");
-            //     var obj = JSON.parse(body);
-            //     var text = obj.response["search"].result.passages[0]["text"];
+                        console.log(text);
+                        console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
-            //     console.log(text);
-            //     console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                        var strippedText = striptags(text);
+                        console.log(strippedText);
 
-            //     var strippedText = striptags(text);
-            //     console.log(strippedText);
-            //     app.tell('Here is the passage: ' + strippedText);
-            // });
+                        return res.json({
+                            speech: speech,
+                            displayText: strippedText,
+                            source: 'apiai-devotion'
+                        });
+                    });
+                }
+            }
         }
 
-        let actionMap = new Map();
-        actionMap.set(GET_PASSAGE, getPassage);
-        app.handleRequest(actionMap);
+        console.log('result: ', speech);
+
+        return null;
     } catch (err) {
         console.error("Can't process request", err);
 
@@ -96,23 +91,24 @@ restService.post('/hook', function (req, res) {
     }
 });
 
-function makeQuery(app) {
-    var book = app.getArgument(BOOK_ARGUMENT);
+function makeQuery(result) {
+    var parameters = result.parameters;
+    var book = parameters[BOOK_ARGUMENT];
     if (!book) {
         return None;
     }
 
-    var chapter = app.getArgument(CHAPTER_ARGUMENT);
+    var chapter = parameters[CHAPTER_ARGUMENT];
     if (!chapter) {
         return None;
     }
 
-    var start_verse = app.getArgument(START_VERSE_ARGUMENT);
+    var start_verse = parameters[START_VERSE_ARGUMENT];
     if (!start_verse) {
         start_verse = "1"
     } 
     
-    var end_verse = app.getArgument(END_VERSE_ARGUMENT);
+    var end_verse = parameters[END_VERSE_ARGUMENT];
     if (!end_verse){
         end_verse = "-ff"
     }
