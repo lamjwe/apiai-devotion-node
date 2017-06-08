849f0c123108f7ac14abb500ee5c58791e0c3ac2
jonathanlam <lamjwe91@gmail.com>
2017-06-07 23:58:53 -0700

    Test rich response with list selector

--- index.js
+++ index.js
@@ -178,28 +178,61 @@ app.post('/', function(req, res, next) {
                     console.log("======== DEBUG ========");
 
                     // Build a list
-                    var list = assistant.buildList('Here are some search results for ' + query);
+                    // var list = assistant.buildList('Here are some search results for ' + query);
                     
-                    resultVerses.forEach(function(verse) {
+                    // resultVerses.forEach(function(verse) {
                         
-                        text = verse.text;
+                    //     text = verse.text;
                         
-                        var strippedText = striptags(text);
-                        console.log("STRIPPED TEXT: " + strippedText);
-                        // resultToDisplay += verse.reference + " \n\n" + strippedText + "\n\n";
+                    //     var strippedText = striptags(text);
+                    //     console.log("STRIPPED TEXT: " + strippedText);
+                    //     // resultToDisplay += verse.reference + " \n\n" + strippedText + "\n\n";
 
-                        // Add the item to the list
-                        list.addItems(assistant.buildOptionItem('MATH_AND_PRIME',
-                        ['math', 'math and prime', 'prime numbers', 'prime'])
-                        .setTitle(verse.reference)
-                        .setDescription(strippedText));
+                    //     // Add the item to the list
+                    //     list.addItems(assistant.buildOptionItem('MATH_AND_PRIME',
+                    //     ['math', 'math and prime', 'prime numbers', 'prime'])
+                    //     .setTitle(verse.reference)
+                    //     .setDescription(strippedText));
                         
-                    });
+                    // });
+
+                    // assistant.askWithList(assistant.buildRichResponse()
+                    //     .addSimpleResponse('Alright, here are your search results for ' + query)
+                    //     .addSuggestions(['Basic Card', 'List', 'Carousel', 'Suggestions']),list
+                    // );
 
                     assistant.askWithList(assistant.buildRichResponse()
-                        .addSimpleResponse('Alright, here are your search results for ' + query)
-                        .addSuggestions(['Basic Card', 'List', 'Carousel', 'Suggestions']),list
+                        .addSimpleResponse('Alright')
+                        .addSuggestions(
+                        ['Basic Card', 'List', 'Carousel', 'Suggestions']),
+                        // Build a list
+                        assistant.buildList('Things to learn about')
+                        // Add the first item to the list
+                        .addItems(assistant.buildOptionItem('MATH_AND_PRIME',
+                        ['math', 'math and prime', 'prime numbers', 'prime'])
+                        .setTitle('Math & prime numbers')
+                        .setDescription('42 is an abundant number because the sum of its ' +
+                            'proper divisors 54 is greater…')
+                        .setImage('http://example.com/math_and_prime.jpg', 'Math & prime numbers'))
+                        // Add the second item to the list
+                        .addItems(assistant.buildOptionItem('EGYPT',
+                        ['religion', 'egpyt', 'ancient egyptian'])
+                        .setTitle('Ancient Egyptian religion')
+                        .setDescription('42 gods who ruled on the fate of the dead in the ' +
+                            'afterworld. Throughout the under…')
+                        .setImage('http://example.com/egypt', 'Egypt')
+                        )
+                        // Add third item to the list
+                        .addItems(assistant.buildOptionItem('RECIPES',
+                        ['recipes', 'recipe', '42 recipes'])
+                        .setTitle('42 recipes with 42 ingredients')
+                        .setDescription('Here\'s a beautifully simple recipe that\'s full ' +
+                            'of flavor! All you need is some ginger and…')
+                        .setImage('http://example.com/recipe', 'Recipe')
+                        )
                     );
+
+                    console.log("DONE");
                 } catch (err) {
                     console.error("ERROR == > ", err);
                     assistant.tell('Sorry, cannot not find the given passage.');
